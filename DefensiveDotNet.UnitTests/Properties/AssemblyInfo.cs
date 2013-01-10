e3a55788c84e3a3bb19058d7a506c80558910fa5
Jeffrey Pratt <jeff@metronomix.com>
2013-01-09 21:52:36 -0700

    Initial commit of site

--- DefensiveDotNet.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UnitTests")]
-[assembly: AssemblyProduct("UnitTests")]
-[assembly: Guid("ed9166c4-660d-4fcc-b2fe-9f31fee331f5")]
